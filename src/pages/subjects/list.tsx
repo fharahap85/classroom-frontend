82e42e11974638a25549b77366d51e01a7f115fd
fharahap85 <82890658+fharahap85@users.noreply.github.com>
2026-04-02 10:48:34 +0700

    Feat: Add search bar, dropdown filter and create button

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,6 +1,61 @@
+import { ListView } from "@/components/refine-ui/views/list-view"
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb"
+import { Search } from "lucide-react"
+import { Input } from "@/components/ui/input"
+import { useState } from "react"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { DEPARTMENT_OPTIONS } from "@/constants"
+import { CreateButton } from "@/components/refine-ui/buttons/create"
+
 const SubjectsList = () => {
+    const [searchQuery, setsearchQuery] = useState('');
+    const [selectedDepartment, setSelectedDepartment] = useState('all');
     return (
-        <div>List</div>
+        <ListView>
+            <Breadcrumb />
+            <h1 className="page-title">Subjects</h1>
+
+            <div className="intro-row">
+                <p>Quick access to essential metrics and management tools.</p>
+
+                <div className="action-row">
+                    <div className="search-field">
+                        <Search className="search-icon" />
+
+                        <Input
+                            type="text"
+                            placeholder="Search by name..."
+                            className="pl-10 w-full"
+                            value={searchQuery}
+                            onChange={(e) => setsearchQuery(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="flex gap-10 w-full sm:w-auto mt-2">
+                        <Select
+                            value={selectedDepartment}
+                            onValueChange={setSelectedDepartment}>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Filter by department" />
+                            </SelectTrigger>
+
+                            <SelectContent>
+                                <SelectItem value="all">
+                                    All Departments
+                                </SelectItem>
+                                {DEPARTMENT_OPTIONS.map(department => (
+                                    <SelectItem key={department.value}
+                                        value={department.value}>
+                                        {department.label}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                        <CreateButton />
+                    </div>
+                </div>
+            </div>
+        </ListView>
     )
 }
 
